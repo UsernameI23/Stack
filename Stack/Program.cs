d1d080603e89a1e84ba6519bb6fa27bba8034bc2
Rayna Miller <89815185+UsernameI23@users.noreply.github.com>
2023-04-30 14:29:59 -0400

    Update Program.cs

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,67 +1,61 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace FunwithStacks
+namespace Stacks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Creates the members Stack
-            Stack<string> members = new Stack<string>();
-            //adding members
-            members.Push("Mickey Mouse");
-            members.Push("Minnie Mouse");
-            // adding more members
-            Console.WriteLine("How many members would you like to add?");
-            int num = int.Parse(Console.ReadLine());
-            // adds data to the members Stack
-            for (int i = 0; i < num; i++)
-            {
-                Console.WriteLine("Member name: ");
-                members.Push(Console.ReadLine());
-            }
-            // Check for an item in the Stack
-            Console.WriteLine("Would you like to look for a member (yes or no)");
+           
+            Stack<string> items = new Stack<string>();
+            items.Push("Violin");
+            items.Push("Viola");
+            items.Push("Cello");
+            items.Push("Bass");
+            items.Push("Harp");
+            
+            Console.WriteLine("Would you like to search our directory for a string instrument to purchase (yes or no)");
             string answer = Console.ReadLine();
             while (answer == "yes")
             {
-                Console.WriteLine("Enter the name of the member you would like to search for");
-                string memberName = Console.ReadLine();
-                if (members.Contains(memberName))
+                Console.WriteLine("Enter the name of the string instrument you would like to search for");
+                string instrumentName = Console.ReadLine();
+                if (items.Contains(instrumentName))
                 {
-                    Console.WriteLine("That member is part of the group.");
+                    Console.WriteLine("That instrument is available.");
                 }
                 else
                 {
-                    Console.WriteLine("I am sorry, that person is not part of the group");
+                    Console.WriteLine("I am sorry, that instrument is not available");
                 }
-                Console.WriteLine("Would you like to look for another member (yes or no)");
+                Console.WriteLine("Would you like to look for another instrument (yes or no)");
                 answer = Console.ReadLine();
             }
-            // counts the members in the Stack
-            Console.WriteLine($"Here are your {members.Count()} members");
-            // displays the Stack contents without removing anything
-            foreach (var m in members)
+            
+            Console.WriteLine($"Here are the available options: {items.Count()} items");
+            
+            foreach (var i in items)
             {
-                Console.WriteLine(m);
+                Console.WriteLine(i);
             }
-            string firstMem = members.Peek();   // retrieves the LAST item
-            Console.WriteLine($"Would you like to remove {firstMem} from the end of the Stack (yes or no)?");
+            string instrument = items.Peek();   
+            Console.WriteLine($"Would you like to remove {instrument} from the end of the Stack (yes or no)?");
             answer = Console.ReadLine();
             while (answer == "yes")
             {
-                members.Pop();  // Removes the LAST item
-                firstMem = members.Peek();
-                Console.WriteLine($"Would you like to remove {firstMem} from the end of the Stack (yes or no)?");
+                items.Pop();  
+                instrument = items.Peek();
+                Console.WriteLine($"Would you like to remove {instrument} from the end of the Stack (yes or no)?");
                 answer = Console.ReadLine();
             }
 
-            if (members.Peek() == null)
+            if (items.Peek() == null)
                 Console.WriteLine("The Stack is empty");
             else
-                Console.WriteLine($"You have {members.Count} left in the Stack");
+                Console.WriteLine($"You have {items.Count} left in the Stack");
 
         }
     }
 }
+
